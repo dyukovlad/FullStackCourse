090176700063bd8bb6b459091facd8c4c0f3ce5b
Vlad Dyukov <dyukovlad@MacBook-Pro-Vlad.local>
2019-09-08 20:34:30 +0300

    destructured

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
-    return (
-        <div>
-        <p>Hello {props.name}, you are {props.age} years old</p>
-        </div>
-    )
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+      <div>
+        <p>Hello {name}, you are {age} years old</p>
+        <p>So you were probably born in {bornYear()}</p>
+      </div>
+  )
 }
   
   const App = () => {
-      
     const name = 'Peter'
-    const age = 10
+    const age = 35
 
     return (
       <div>
         <h1>Greetings</h1>
-        <Hello name="George" age={21+age}/>
+        <Hello name="George" age={21}/>
         <Hello name={name} age={age} />
       </div>
     )
