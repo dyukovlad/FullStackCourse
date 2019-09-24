3136863c61b9662768038801bd17bc05ab3069c6
Vlad Dyukov <dyukovlad@MacBook-Pro-Vlad.local>
2019-09-24 17:35:29 +0300

    2.2: Course contents step7

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Course from './components/Course'
 
 const App = ({ course }) => {
-    console.log(course.parts);
-
+    // console.log(course.parts);
     const rows = () => course.parts.map(parts =>
         <Course  key={parts.id} course={parts} />
     )
+
+    const summ = () => course.parts.reduce((acc, sum) => {
+        return acc + sum.exercises;
+    }, 0)
 
   return (
     <div>
       <ul>
         {rows()}
+        <p><strong>total of {summ()} exercises</strong></p>
       </ul>
     </div>
   )
